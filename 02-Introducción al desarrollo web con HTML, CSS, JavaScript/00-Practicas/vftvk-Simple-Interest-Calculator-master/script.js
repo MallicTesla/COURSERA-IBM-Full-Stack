8c26c62108723f6ceee07c54ad17274e02803561
Mallic Tesla <el_mallic@hotmail.com>
2023-11-14 00:28:09 -0300

    ahora si termine la practica

--- 02-Introducción al desarrollo web con HTML, CSS, JavaScript/00-Practicas/vftvk-Simple-Interest-Calculator-master/script.js
+++ 02-Introducción al desarrollo web con HTML, CSS, JavaScript/00-Practicas/vftvk-Simple-Interest-Calculator-master/script.js
@@ -1,24 +1,24 @@
-function updateRate() {
+function actualizar_ratio() {
     var valor_ratio = document.getElementById("ratio").value;           //  esto toma el balor de la etiqueta con el id "ratio"
-    document.getElementById("rate_val").innerText = valor_ratio;        //  esto envia el valor de la variable "valor_ratio" a la etiqueta con el id "rate_val"
+    document.getElementById("valor_ratio").innerText = valor_ratio;        //  esto envia el valor de la variable "valor_ratio" a la etiqueta con el id "valor_ratio"
 }
 
-function compute(){
+function calcular(){
     var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value; 
-    var interest = principal * years * rate / 100;
-    var amount = parseInt(principal) + parseFloat(interest);
-    var result = document.getElementById("result");
+    var ratio = document.getElementById("ratio").value;
+    var años = document.getElementById("años").value; 
+    var interes = principal * años * ratio / 100;
+    var cantidad = parseInt(principal) + parseFloat(interes);
+    var resultado = document.getElementById("resultado");
     
-    var year = new Date().getFullYear() + parseInt(years);              //  toma loas años que pusistes y los suma al año actual
+    var year = new Date().getFullYear() + parseInt(años);              //  toma loas años que pusistes y los suma al año actual
     
     if (principal <= 0) {
         alert("Por fabor ingrese un numero positivo !");
         document.getElementById("principal").focus();
     }
     else {                                                              // lo que esta entre las etiquetas "mark" se resalta
-        result.innerHTML = "If you deposit $" + "<mark>" + principal + "</mark>" + ",\<br\> at an interest rate of " + "<mark>" + rate + "%" + "</mark>" + "\<br\> You will receive an amount of $" + "<mark>" + amount + "</mark>" + ",\<br\> in the year " + "<mark>" + year + "</mark>" + "\<br\>";
+        resultado.innerHTML = "Si depositastes $" + "<mark>" + principal + "</mark>" + ",\<br\> a una tasa de interés del " + "<mark>" + ratio + "%" + "</mark>" + "\<br\> Tendras en total $" + "<mark>" + cantidad + "</mark>" + ",\<br\> para el año " + "<mark>" + year + "</mark>" + "\<br\>";
     }
 }
 
